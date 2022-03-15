5102f43e12ec94bffa6a868e1c301bd816c925b9
MGVK <MukyLike@yandex.ru>
2022-03-15 19:18:43 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -10,14 +10,11 @@ class LocalRedirect:
 		ctx.log.info("pretty host is: %s" % flow.request.pretty_host)
 		newhost=flow.request.host.replace('.','-')+".translate.goog"
 		flow.request.path="/?_x_tr_sl=en&_x_tr_tl=ru&_x_tr_hl=ru&_x_tr_pto=wapp"
-		ctx.log.info("pretty host is: %s" % flow.request.path)
 		flow.request.host = newhost
 		flow.request.headers["Host"] = newhost
 		flow.request.port = 443
 		flow.request.scheme = "https"
 		ctx.log.info("pretty host is: %s" % flow.request)
-
-#    var url = `https://translate.google.com/translate?anno=2&depth=1&sp=nmt4&sl=en&tl=fr&u=${info.url}`;
 addons = [
 	LocalRedirect()
 ]
